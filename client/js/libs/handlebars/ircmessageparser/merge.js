cc7d121c03e95e0554e6fe81dbc066fbea9cd7cb
Jérémie Astori <jeremie@astori.fr>
2017-04-18 00:53:32 -0400

    Remove Object.assign polyfill and use native one instead

--- client/js/libs/handlebars/ircmessageparser/merge.js
+++ client/js/libs/handlebars/ircmessageparser/merge.js
@@ -3,28 +3,14 @@
 const anyIntersection = require("./anyIntersection");
 const fill = require("./fill");
 
-let Object_assign = Object.assign;
-
-if (typeof Object.assign !== "function") {
-	Object_assign = function(target) {
-		Array.prototype.slice.call(arguments, 1).forEach(function(obj) {
-			Object.keys(obj).forEach(function(key) {
-				target[key] = obj[key];
-			});
-		});
-		return target;
-	};
-}
-
 function assign(textPart, fragment) {
-	const fragStart = fragment.start;
 	const start = Math.max(fragment.start, textPart.start);
 	const end = Math.min(fragment.end, textPart.end);
 
-	return Object_assign({}, fragment, {
+	return Object.assign({}, fragment, {
 		start: start,
 		end: end,
-		text: fragment.text.slice(start - fragStart, end - fragStart)
+		text: fragment.text.slice(start - fragment.start, end - fragment.start)
 	});
 }
 
