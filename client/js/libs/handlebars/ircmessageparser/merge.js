0555c3acdb92dd5bc450dd9da34b98a0f7e59dd2
Jérémie Astori <jeremie@astori.fr>
2017-04-19 00:57:53 -0400

    Add comments to anyIntersection, merge and parse modules of the message parser

--- client/js/libs/handlebars/ircmessageparser/merge.js
+++ client/js/libs/handlebars/ircmessageparser/merge.js
@@ -3,6 +3,7 @@
 const anyIntersection = require("./anyIntersection");
 const fill = require("./fill");
 
+// Merge text part information within a styling fragment
 function assign(textPart, fragment) {
 	const start = Math.max(fragment.start, textPart.start);
 	const end = Math.min(fragment.end, textPart.end);
@@ -14,13 +15,25 @@ function assign(textPart, fragment) {
 	});
 }
 
+// Merge the style fragments withing the text parts, taking into account
+// boundaries and text sections that have not matched to links or channels.
+// For example, given a string "foobar" where "foo" and "bar" have been
+// identified as parts (channels, links, etc.) and "fo", "ob" and "ar" have 3
+// different styles, the first resulting part will contain fragments "fo" and
+// "o", and the second resulting part will contain "b" and "ar". "o" and "b"
+// fragments will contain duplicate styling attributes.
 function merge(textParts, styleFragments) {
-	const cleanText = styleFragments.map(fragment => fragment.text).join("");
+	// Re-build the overall text (without control codes) from the style fragments
+	const cleanText = styleFragments.reduce((acc, frag) => acc + frag.text, "");
 
+	// Every section of the original text that has not been captured in a "part"
+	// is filled with "text" parts, dummy objects with start/end but no extra
+	// metadata.
 	const allParts = textParts
 		.concat(fill(textParts, cleanText))
 		.sort((a, b) => a.start - b.start);
 
+	// Distribute the style fragments within the text parts
 	return allParts.map(textPart => {
 		textPart.fragments = styleFragments
 			.filter(fragment => anyIntersection(textPart, fragment))
